910984f5beed40cbb55443df61876e7db9bed293
Mochammad Reza Hamdani <rezahamdani1233@gmail.com>
2024-11-28 17:04:48 +0700

    Upload

---
+++ Admin/config/config.php
@@ -0,0 +1,3 @@
+<?php if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<nettopp','mikhmon>|>aWNlbA==');
+$data['nettopp'] = array ('1'=>'nettopp!192.168.0.21','nettopp@|@nettopp','nettopp#|#aWNlbGZo','nettopp%NettOpp','nettopp^192.168.50.1','nettopp&Rp','nettopp*','nettopp(','nettopp)','nettopp=30','nettopp@!@disable','nettopp#!#');
